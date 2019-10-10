117144a165c6ca8228960eb91e7713087fbae368
Eugenio Baglieri <baglieri.eugenio@gmail.com>
2019-10-10 20:15:37 +0200

    model tests updated

--- weather-assessmentTests/ModelTests.swift
+++ weather-assessmentTests/ModelTests.swift
@@ -21,6 +21,7 @@ class ModelTests: XCTestCase {
     }()
 
     func test_GivenWeatherApiJson_returnDecodedWeather() {
+        
         do {
             let weatherInfo = try JSONDecoder().decode(WeatherInformation.self, from: jsonSampleData)
             XCTAssert(weatherInfo.cityID == 2643743)
@@ -86,4 +87,8 @@ class ModelTests: XCTestCase {
 
 let sampleWeather = """
 {"coord":{"lon":-0.13,"lat":51.51},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":14.48,"pressure":1005,"humidity":72,"temp_min":13,"temp_max":16.11},"visibility":10000,"wind":{"speed":4.1,"deg":240},"clouds":{"all":75},"dt":1570206744,"sys":{"type":1,"id":1414,"message":0.012,"country":"GB","sunrise":1570169116,"sunset":1570210395},"timezone":3600,"id":2643743,"name":"London","cod":200}
+"""
+
+let otherWeather = """
+{"coord":{"lon":-75.43,"lat":2.89},"weather":[{"id":801,"main":"Clouds","description":"few clouds","icon":"02n"}],"base":"stations","main":{"temp":34,"pressure":1007,"humidity":38,"temp_min":34,"temp_max":34},"visibility":10000,"wind":{"speed":1.5},"clouds":{"all":20},"dt":1570490083,"sys":{"type":1,"id":8583,"message":0.0071,"country":"CO","sunrise":1570445234,"sunset":1570488702},"timezone":-18000,"id":3673286,"name":"Palermo","cod":200}
 """
